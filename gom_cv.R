3a367c484e365872169b94c961b5f75f9adb3e83
Steven Whitlow <54036907+stevenwhitlow@users.noreply.github.com>
2021-01-04 14:21:39 -0400

    estimation process as functions

--- gom_cv.R
+++ gom_cv.R
@@ -17,79 +17,31 @@ max_elbo_test <- array(0, dim = c(num_types))
 
 #list_initial_vals_alpha <-c(5/3, 6/3, 1/3, 6/3, 1/3, 3/3, 1/3)
 #list_initial_vals_theta <-c(4/3, 2/3, 5/3, 3/3, 5/3, 1/3, 3/3)
-Total <- 968
-# Number of variables
-J <- 128
-# we only have one replicate for each of the variables
-Rj <- rep(1, J)
-Vj <- rep(3, J)
-dist <- rep("multinomial", J)
+
 num_completed <- 0 
 
 training.samples <- tasks_wide$`Active Learning` %>% createFolds(k=num_folds, list = FALSE)
 
+#Total number of runs -> (num_types-1)*Total_seeds * total_alpha * total_theta * num_folds
 for (seed in 1:total_seeds){
-  set.seed(seed_array[seed])
+ #skip degenerate model with K=1:
 for (type_number in 2:num_types){
-  K <- type_number
 for (num_alpha in 1:total_alpha){
-  alpha <- rep(initial_alpha[num_alpha], K)
 for (num_theta in 1:total_theta){
 for (i in 1:num_folds){
   num_completed <- num_completed + 1
-print(paste0("Model: ", num_completed, "/", (7 - 1)*(total_seeds)*(total_alpha)*(total_theta)*(num_folds), ". Seed: ", seed, ". Number of topics: ", type_number, ". Theta: ", initial_theta[num_theta], "Alpha: ", initial_alpha[num_alpha], ". Testing set is fold ", i, "/", num_folds, "."))
-tasks_wide %>% filter(training.samples != i) -> train.data
-tasks_wide %>% filter(training.samples == i) -> test.data
-TotalTrain <- nrow(train.data)
-TotalTest <- nrow(test.data)
-Nijr_train <- array(1, dim = c(TotalTrain, J, max(Rj)))
-Nijr_test <- array(1, dim = c(TotalTest, J, max(Rj)))
+print(paste0("Model: ", num_completed, "/", (num_types - 1)*(total_seeds)*(total_alpha)*(total_theta)*(num_folds), ". Seed: ", seed, ". Number of topics: ", type_number, ". Theta: ", initial_theta[num_theta], "Alpha: ", initial_alpha[num_alpha], ". Testing set is fold ", i, "/", num_folds, "."))
+tasks_wide %>% select(-c(1:2)) %>% filter(training.samples != i) -> train_data
+tasks_wide %>% select(-c(1:2)) %>% filter(training.samples == i) -> test_data
 
-theta <- array(0, dim = c(J, K, max(Vj)))
-for (j in 1:J) {
-  theta[j, , ] <- gtools::rdirichlet(K, rep(initial_theta[num_theta], Vj[j]))
-}
+total_train <- nrow(train_data)
+total_test <- nrow(test_data)
 
-# There are 3 choices for each of the variables ranging from 0 to 2.
-# All variables are multinomial
+elbo_test[type_number,seed,num_theta,num_alpha,i]  <- cv_fold_elbo(train_data, test_data, 
+                                                                   total_train, total_test,
+                                                                   128,
+                                                                   type_number, initial_alpha[num_alpha], initial_theta[num_theta], seed_array[seed])
 
-# obs are the observed responses. it is a 4-d array indexed by i,j,r,n
-# note that obs ranges from 0 to 2 for each response
-obsTrain <- array(0, dim = c(TotalTrain, J, max(Rj), max(Nijr_train)))
-obsTest <- array(0, dim = c(TotalTest, J, max(Rj), max(Nijr_test)))
-train.data %>% select(-c(1:2)) %>% as.matrix -> obsTrain[, , 1, 1]
-test.data %>% select(-c(1:2)) %>% as.matrix -> obsTest[, , 1, 1]
-
-# Create the mixedMemModel
-# This object encodes the initialization points for the variational EM algorithim
-# and also encodes the observed parameters and responses
-start <- proc.time()
-initial <- mixedMemModelVarInf(Total = TotalTrain, J = J, Rj = Rj,
-                         Nijr = Nijr_train, K = K, Vj = Vj, alpha = alpha,
-                         theta = theta, dist = dist, obs = obsTrain)
-computeELBO(initial)
-#printStatus 1 indicates that status updates will be printed
-# printMod 25 indicates that status updates will only be printed ever 25th step
-out <- mmVarInfFit(initial, printStatus = 1, printMod = 25)
-computeELBO(out)
-
-#Can run again for 2-step process (see AOAS 2015 paper)
-#initial <- mixedMemModelVarInf(Total = TotalTrain, J = J, Rj = Rj,
-#                               Nijr = Nijr_train, K = K, Vj = Vj, alpha = out$alpha,
-#                               theta = out$theta, dist = dist, obs = obsTrain)
-#computeELBO(initial)
-#out <- mmVarInfFit(initial, printStatus = 1, printMod = 25)
-#computeELBO(out)
-
-initial_test <- mixedMemModelVarInf(Total = TotalTest, J = J, Rj = Rj,
-                         Nijr = Nijr_test, K = K, Vj = Vj, alpha = out$alpha,
-                         theta = out$theta, dist = dist, obs = obsTest)
-#printStatus 1 indicates that status updates will be printed
-# printMod 25 indicates that status updates will only be printed ever 25th step
-out_test <- mmVarInfFit(initial_test, printStatus = 1, printMod = 25, stepType=0)
-end <- proc.time()
-elbo_test[type_number,seed,num_theta,num_alpha,i] <- computeELBO(out_test)
-print(end - start)
 }
 #mean_elbo_test[type_number,num_theta,num_alpha] = mean(elbo_test[type_number,num_theta,num_alpha,])
 }}
@@ -97,7 +49,14 @@ print(end - start)
 }
 }
 
-elbo_df <- melt(data = elbo_test, varnames=c("NumTypes","Seed", "InitializationTheta", "InitializationAlpha", "NumFold"), value.name="ELBO")
-elbo_df %>% group_by(NumTypes,Seed, InitializationTheta, InitializationAlpha) %>% mutate(meanInitial = mean(ELBO)) -> elbo_df
-elbo_df %>% group_by(NumTypes) %>% filter(NumTypes!=1) %>% slice_max(order_by = meanInitial, n=1, with_ties= FALSE) %>% select(NumTypes, InitializationTheta, InitializationAlpha, Seed, meanInitial)
+elbo_df <- melt(data = elbo_test, varnames=c("NumTypes","Seed", "InitializationTheta", "InitializationAlpha", "NumFold"), value.name="ELBO") %>%
+    group_by(NumTypes,Seed, InitializationTheta, InitializationAlpha) %>%
+    filter(NumTypes!=1) %>%
+    mutate(meanInitial = mean(ELBO))
+
 write.csv(elbo_df, "elbo_kfold_initializations.csv")
+
+elbo_df %>% group_by(NumTypes) %>%
+    slice_max(order_by = meanInitial, n=1, with_ties= FALSE) %>%
+    select(NumTypes, InitializationTheta, InitializationAlpha, Seed, meanInitial) %>%
+    write.csv("elbo_kfold_best_initializations.csv")
---
+++ gom_estimation.R
@@ -0,0 +1,138 @@
+library(tidyverse)
+
+clean_for_gom <- function() {
+
+    library(readxl)
+
+    # Keep only the occupation codes and titles and the task names and values
+    keep <- c("O*NET-SOC Code", "Title", "Element Name", "Data Value")
+
+    # For each category: read in from Excel file
+    # Keep only the level scale for each task, and keep only the columns in the "keep" vector
+
+    Abilities <- read_excel("./data/Abilities.xlsx")
+    Abilities <- subset(Abilities, `Scale ID`=="LV")
+    Abilities <- Abilities[, names(Abilities) %in% keep, drop = F]
+
+    Skills <- read_excel("./data/Skills.xlsx")
+    Skills <- subset(Skills, `Scale ID`=="LV")
+    Skills <- Skills[, names(Skills) %in% keep, drop = F]
+
+    Activities <- read_excel("./data/Activities.xlsx")
+    Activities <- subset(Activities, `Scale ID`=="LV")
+    Activities <- Activities[, names(Activities) %in% keep, drop = F]
+
+    tasks <<- bind_rows(Abilities, Skills, Activities)
+    tasks_wide <<- tasks %>% spread(`Element Name`,`Data Value`)
+
+    for (j in 3:ncol(tasks_wide)) {
+      tasks_wide[[colnames(tasks_wide %>% select(c(j)))]] <<- ntile(tasks_wide[[colnames(tasks_wide %>% select(c(j)))]],3) - 1
+    }
+
+    gom_data <<- tasks_wide %>% select(-c(1:2))
+}
+
+estimate_gom <- function(data, total, J, K, initial_alpha, initial_theta, seed, permutation = FALSE, returnELBO = FALSE) {
+    library(mixedMem)
+    library(gtools)
+
+    # Vector of responses for each task
+    Rj <- rep(1, J)
+
+    # Matrix of responses for each observation and task
+    Nijr <- array(1, dim = c(total, J, max(Rj)))
+
+    # Each task takes values from 0-2.
+    Vj <- rep(3, J)
+
+    #Propagate data to matrix of observations
+    obs <- array(0, dim = c(total, J, max(Rj), max(Nijr)))
+    data %>% as.matrix -> obs[, , 1, 1]
+
+
+    # Each variable is multinomial
+    dist <- rep("multinomial", J)
+
+    alpha <- rep(initial_alpha, K)
+
+    # Initialize theta randomly with Dirichlet distributions
+    set.seed(seed)
+    theta <- array(0, dim = c(J, K, max(Vj)))
+    for (j in 1:J) {
+        theta[j, , ] <- rdirichlet(K, rep(initial_theta, Vj[j]))
+    }
+
+    #
+    model_starting_values <- mixedMemModelVarInf(Total = total, J = J, Rj = Rj,
+                                                 Nijr = Nijr, K = K, Vj = Vj, alpha = alpha,
+                                                 theta = theta, dist = dist, obs = obs)
+    computeELBO(model_starting_values)
+    output_model <- mmVarInfFit(model_starting_values, printStatus = 1, printMod = 10)
+    computeELBO(output_model)
+    if(isTRUE(permutation)) {
+        output_model <- permuteLabels(permutation)
+    }
+
+    if(isTRUE(returnELBO)) {
+        return(computeELBO(output_model))
+    } else {
+        return(output_model)
+    }
+}
+
+fit_gom <- function(data, total, J, K, alpha, theta) {
+    library(mixedMem)
+
+    Rj <- rep(1, J)
+    Nijr <- array(1, dim = c(total, J, max(Rj)))
+
+    # Each task takes values from 0-2.
+    Vj <- rep(3, J)
+
+    obs <- array(0, dim = c(total, J, max(Rj), max(Nijr)))
+    data %>% as.matrix -> obs[, , 1, 1]
+
+    dist <- rep("multinomial", J)
+
+    model_starting_values <- mixedMemModelVarInf(Total = total, J = J, Rj = Rj,
+                                   Nijr = Nijr, K = K, Vj = Vj, alpha = alpha,
+                                   theta = theta, dist = dist, obs = obs)
+    computeELBO(model_starting_values)
+    out <- mmVarInfFit(model_starting_values, printStatus = 1, stepType = 0)
+    computeELBO(out)
+    return(out)
+}
+
+cv_fold_elbo <- function(dataTrain, dataTest, totalTrain, totalTest, J, K, initial_alpha, initial_theta, seed) {
+    library(mixedMem)
+
+    out <- estimate_gom(dataTrain, totalTrain, J, K, initial_alpha, initial_theta, seed)
+    #Can run again for 2-step process (see AOAS 2015 paper)
+
+    out_test_ELBO <- fit_gom(dataTest, totalTest, J, K, out$alpha, out$theta) %>%
+                     computeELBO()
+
+    return(out_test_ELBO)
+}
+
+extract_classifications <- function(gom_output, filename, return_classifications = FALSE) {
+    lambda <- as.data.frame.table((gom_output$phi) / (rowSums(gom_output$phi)))
+    lambda <- rename(lambda, occnumber=Var1)
+    lambda <- rename(lambda, group=Var2)
+
+    tasks_wide %>% select(c(1,2)) -> occupations
+    occupations$occnumber = paste0("Ind",1:nrow(occupations))
+
+    classifications <- inner_join(occupations, lambda)
+    classifications$group <- substr(classifications$group, 6,6)
+    classifications <- classifications %>% spread(group, Freq)
+    for (num_group in 1:(gom_output$K)) {
+        classifications <- rename(classifications, !!paste0("freq", num_group) := !!paste0("", num_group, ""))
+    }
+    if(!missing(filename)){
+        write.csv(classifications, filename)
+    }
+    if(return_classifications == TRUE){
+        return(classifications)
+    }
+}
